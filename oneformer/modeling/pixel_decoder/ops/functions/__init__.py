f5dd2f2889704b98dc865022e6be3ffd5bbf05f8
guillaume-rochette-oxb <guillaume.rochette@oxbotica.com>
2023-12-18 11:43:00 +0000

    Removed more stuff related to MultiScaleDeformableAttention.

--- oneformer/modeling/pixel_decoder/ops/functions/__init__.py
+++ oneformer/modeling/pixel_decoder/ops/functions/__init__.py
@@ -5,5 +5,3 @@
 # ------------------------------------------------------------------------------------------------
 # Modified from https://github.com/chengdazhi/Deformable-Convolution-V2-PyTorch/tree/pytorch_1.0.0
 # ------------------------------------------------------------------------------------------------
-
-from .ms_deform_attn_func import MSDeformAttnFunction
